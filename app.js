73d01ed08d0ee4f9e4de42a6a6ca949225112f51
Bonte7 <cjbonte7@gmail.com>
2021-11-05 11:13:06 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const request = require("request");
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//get request for the signup homepage
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html")
+});
+
+//post request for the signup form
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+//sotring the required member info for mailchimp
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+//converting the data variable from JavaScript to JSON
+  var jsonData = JSON.stringify(data);
+
+//url for all post requests
+  const url = "https://us20.api.mailchimp.com/3.0/lists/62db71e502"
+
+  const options = {
+    method: "POST",
+    auth: "christianbonte:70fd3f25f0118ac37d74c45979ce5583-us20"
+  }
+
+//request to send members to mailcchimp
+  const request = https.request(url, options, (response) => {
+
+    if (response.statusCode === 200) {
+      res.send("Successfully subscribed!");
+    }
+    else {
+      res.send("There was an error signing you up!");
+    }
+
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+request.write(jsonData);
+request.end();
+
+});
+
+//running server on port 3000
+app.listen(port, () => {
+  console.log("Running on port " + port);
+});
+
+//Mailchimp API key
+//70fd3f25f0118ac37d74c45979ce5583-us20
+
+//Mailchimp List ID
+//62db71e502
